6eb9a69ac89034159b8693ba320a67db755d004e
MohammedRashad1 <A-10563@ibsplc.com>
2022-02-24 19:49:44 +0530

    first commit

---
+++ src/main/java/com/ibs/training/springboot/SpringBootSampleTesting/SpringBootSampleTestingApplication.java
@@ -0,0 +1,13 @@
+package com.ibs.training.springboot.SpringBootSampleTesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSampleTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSampleTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibs/training/springboot/SpringBootSampleTesting/SpringBootSampleTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibs.training.springboot.SpringBootSampleTesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSampleTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
